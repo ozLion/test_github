dd53deeb32c25d1b9b2ec3f48312cdbcaaaa1f59
ozLion <dhpark815@gmail.com>
2026-03-21 23:53:06 +0900

    Add sum() function

--- hello_world.c
+++ hello_world.c
@@ -2,7 +2,13 @@
 
 #include <stdio.h>
 
+int sum(int a, int b) {
+    return a + b;
+}
+
 int main() {
     printf("Hello, World!\n");
+
+    printf("sum(3, 4) = %d\n", sum(3, 4));
     return 0;
 }
