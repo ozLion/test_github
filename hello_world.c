60808a55569f352a644f155cb6e31f6377430784
ozLion <dhpark815@gmail.com>
2026-03-22 14:14:47 +0900

    사칙연산으로 확장

--- hello_world.c
+++ hello_world.c
@@ -2,13 +2,32 @@
 
 #include <stdio.h>
 
-int sum(int a, int b) {
+int add(int a, int b) {
     return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0; // or handle error
+    }
 }
 
 int main() {
     printf("Hello, World!\n");
 
-    printf("sum(3, 4) = %d\n", sum(3, 4));
+    printf("add(3, 4) = %d\n", add(3, 4));
+    printf("subtract(3, 4) = %d\n", subtract(3, 4));
+    printf("multiply(3, 4) = %d\n", multiply(3, 4));
+    printf("divide(3, 4) = %d\n", divide(3, 4));
     return 0;
 }
